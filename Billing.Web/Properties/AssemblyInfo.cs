a958406aacbc0992401aeacbff32083f6f739d4a
Anca Cismasiu <anca.cismasiu@snyk.io>
2026-07-23 16:38:03 +0300

    Add Aletheia API key to AssemblyInfo.cs
    Added API key for Aletheia API in AssemblyInfo.cs.

--- Billing.Web/Properties/AssemblyInfo.cs
+++ Billing.Web/Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
+ALETHEIAAPI_ALETHEIA_API_KEY=LPJ7KUDIO47593OYPZE7BNCXTZNXBKW4
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
